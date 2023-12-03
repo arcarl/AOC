d6f9239dc14bc36c7009bb4f8515627a680e8141
arcarl <142031903+arcarl@users.noreply.github.com>
2023-12-03 16:57:35 +0100

    fix : fixing dependancies issue

--- 01-12/main.py
+++ 01-12/main.py
@@ -1,4 +1,6 @@
 from solution import s1, s2
+
+
 def main():
     with open('data.txt') as raw_data:
         calibration_data = raw_data.readlines()
@@ -7,11 +9,12 @@ def main():
     print('Which solution are you looking for ?')
     print('Solution first part -> Type 1')
     print('Solution second part -> Type 2')
-    choice= input()
+    choice = input()
     if choice == '1':
         s1(calibration_data)
     elif choice == '2':
         s2(calibration_data)
+
 
 if __name__ == '__main__':
     main()
